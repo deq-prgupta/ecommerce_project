f5f9a3a79fa09b55613277d83f60a79a1e814689
deq <prgupta@deqode.com>
2022-07-20 15:35:08 +0530

    Initial Commit

--- ecommerce_project/urls.py
+++ ecommerce_project/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from .views import Home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('authentication.urls')),
+    # path('accounts/', include('authentication.urls')),
     
-    # path('accounts/', include('allauth.urls')),
+    path('accounts/', include('allauth.urls')),
+    path('', Home.as_view(), name='home')
 ]
